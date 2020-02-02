57f89809e7d0154e89adcad911c8e855fec69445
Lucas Souza <lucassouza690@gmail.com>
2020-02-02 08:39:50 -0300

    Acrescentando novas views

--- Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
+++ Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
@@ -12,5 +12,15 @@ namespace CasaDoCodigo.Controllers
         {
             return View();
         }
+
+        public IActionResult Carrinho()
+        {
+            return View();
+        }
+
+        public IActionResult Resumo()
+        {
+            return View();
+        }
     }
 }
