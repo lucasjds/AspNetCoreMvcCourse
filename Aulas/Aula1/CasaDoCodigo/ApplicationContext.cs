b6a6f17ad24c3023aff638c849e39442b030d936
Lucas Souza <lucassouza690@gmail.com>
2020-02-16 18:49:44 -0300

    Conexao com banco e alimentando o DB

---
+++ Aulas/Aula1/CasaDoCodigo/ApplicationContext.cs
@@ -0,0 +1,32 @@
+﻿using CasaDoCodigo.Models;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext( DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Produto>().HasKey(x => x.Id);
+            modelBuilder.Entity<Pedido>().HasKey(x => x.Id);
+            modelBuilder.Entity<Pedido>().HasMany(x => x.Itens).WithOne(x => x.Pedido);
+            modelBuilder.Entity<Pedido>().HasOne(x => x.Cadastro).WithOne(x => x.Pedido).IsRequired();
+            modelBuilder.Entity<ItemPedido>().HasKey(x => x.Id);
+            modelBuilder.Entity<ItemPedido>().HasOne(x => x.Pedido);
+            modelBuilder.Entity<ItemPedido>().HasOne(x => x.Produto);
+            modelBuilder.Entity<Cadastro>().HasKey(x => x.Id);
+            modelBuilder.Entity<Cadastro>().HasOne(x => x.Pedido);
+        }
+    }
+}
--- Aulas/Aula1/CasaDoCodigo/Startup.cs
+++ Aulas/Aula1/CasaDoCodigo/Startup.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CasaDoCodigo.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -22,10 +24,16 @@ namespace CasaDoCodigo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            string connectionString = Configuration.GetConnectionString("Default");
+            services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connectionString));
+
+            services.AddTransient<IDataService,DataService>();
+            services.AddTransient<IProdutoRepository, ProdutoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -45,6 +53,8 @@ namespace CasaDoCodigo
                     name: "default",
                     template: "{controller=Pedido}/{action=Carrossel}/{id?}");
             });
+
+            serviceProvider.GetService<IDataService>().InicializaDB();
         }
     }
 }
