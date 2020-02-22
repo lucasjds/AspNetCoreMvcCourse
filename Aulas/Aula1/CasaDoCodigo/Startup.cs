973e80b95ee4030974dc295205c94b2a626a8069
Lucas Souza <lucassouza690@gmail.com>
2020-02-22 18:00:08 -0300

    Criando Interfaces e Repositories para as outras classes

--- Aulas/Aula1/CasaDoCodigo/Startup.cs
+++ Aulas/Aula1/CasaDoCodigo/Startup.cs
@@ -30,6 +30,10 @@ namespace CasaDoCodigo
 
             services.AddTransient<IDataService,DataService>();
             services.AddTransient<IProdutoRepository, ProdutoRepository>();
+            services.AddTransient<ICadastroRepository, CadastroRepository>();
+            services.AddTransient<IPedidoRepository, PedidoRepository>();
+            services.AddTransient<IItemPedidoRepository, ItemPedidoRepository>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
